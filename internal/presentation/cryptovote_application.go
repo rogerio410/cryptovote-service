831e62d6df3ea8b7f47214b7d1386b574b9b69e9
Rogerio Silva <rogerio410@gmail.com>
2021-10-31 18:25:14 -0300

    MongoDB server URI fix

--- internal/presentation/cryptovote_application.go
+++ internal/presentation/cryptovote_application.go
@@ -45,9 +45,8 @@ func newMongoDBClient(ctx context.Context) *mongo.Client {
 
 	mongodbUri := ""
 	if user != "" {
-		mongodbUri = fmt.Sprintf("mongodb+srv://%v:%v@%v/cryptovote?retryWrites=true&w=majority&ssl=true", user, password, host)
+		mongodbUri = fmt.Sprintf("mongodb+srv://%v:%v@%v/test?authSource=admin&replicaSet=atlas-5a4p0u-shard-0&readPreference=primary&appname=MongoDBCompass&ssl=true", user, password, host)
 	} else {
-
 		mongodbUri = fmt.Sprintf("mongodb://%v:%v/cryptovote", host, port)
 	}
 
